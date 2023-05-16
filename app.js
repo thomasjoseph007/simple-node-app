72cbdddacf6da0899bf2cfdf133e7d7ccbd1dc87
Thomas Joseph <33921393+thomasvjoseph@users.noreply.github.com>
2023-05-16 12:19:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get(' ',(req,res) => {
+app.get('/',(req,res) => {
     res.send('Hello World! from Node.Js tt');
 });
 app.listen('8080','0.0.0.0',() =>{
