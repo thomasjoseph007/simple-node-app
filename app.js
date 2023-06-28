29b1f42eef7831dab2dc9ba56531d3314c076194
Thomas Joseph <33921393+thomasvjoseph@users.noreply.github.com>
2023-06-28 16:55:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello World! from Node.Js TJ qwes new');
+    res.send('Hello World! from Node.Js TJ QWERTY');
 });
 app.listen('8080','0.0.0.0',() =>{
     console.log('listening to port 80 work ');
